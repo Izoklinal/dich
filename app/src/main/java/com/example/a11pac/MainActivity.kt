8fca66409b5249c794c60ae34017fcea606573e1
Izoklinal <myggga@mail.ru>
2022-09-16 09:57:55 +0500

    2

--- app/src/main/java/com/example/a11pac/MainActivity.kt
+++ app/src/main/java/com/example/a11pac/MainActivity.kt
@@ -1,16 +1,68 @@
 package com.example.a11pac
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+    private var bookList: MutableList<Expenses> = mutableListOf()
+    private lateinit var name: EditText
+    private lateinit var cost: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    val btn = findViewById<Button>(R.id.add_button)
+        getExpense()
+        val btn_add = findViewById<Button>(R.id.add_button)
+        val btn_show = findViewById<Button>(R.id.show_button)
+        val name = findViewById<EditText>(R.id.editText_name)
+        val cost = findViewById<EditText>(R.id.editText_cost)
 
-    //val expense: Expenses = Gson().fromJson("expenses.json", Expenses)
+        btn_add.setOnClickListener(View.OnClickListener {
+            if (name.text.toString()!="" && cost.text.toString()!="")
+            {
+                //val temp: Expenses = Expenses(name.text.toString(), cost.text.toString().toInt())
+                addExpense(name.text.toString(), cost.text.toString().toInt())
+                Toast.makeText(this, "Успешно добавлено", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this, "Нельзя добавить пустую строку", Toast.LENGTH_SHORT).show()
+            }
+        })
+        btn_show.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, ShowActivity::class.java)
+            startActivity(intent)
+        })
+    }
+    private fun getExpense(){
+        val preferences = getSharedPreferences("pref", MODE_PRIVATE)
+        var json: String = ""
+        if (!preferences.contains("json"))
+        {
+            return
+        } else {
+            json = preferences.getString("json", "NOT_JSON").toString()
+        }
+        val temp = Gson().fromJson<List<Expenses>>(json, object: TypeToken<List<Expenses>>(){}.type)
+        bookList.addAll(temp)
+    }
+
+    private fun addExpense(name: String, cost: Int)
+    {
+        val temp: Expenses = Expenses(name, cost)
+        bookList.add(temp)
+        val preferences = getSharedPreferences("pref", MODE_PRIVATE)
+        preferences.edit {
+            this.putString("json", Gson().toJson(bookList).toString())
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/a11pac/ShowActivity.kt
@@ -0,0 +1,38 @@
+package com.example.a11pac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class ShowActivity : AppCompatActivity() {
+    private var bookList: MutableList<Expenses> = mutableListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+        var all_text = findViewById<TextView>(R.id.textView_all)
+        getExpense()
+        bookList.forEach{
+            Log.d("YO", it.toString())
+            all_text.text = bookList.toString()
+            Toast.makeText(this, "yes", Toast.LENGTH_SHORT).show()
+            Log.d("YO", bookList.toString())
+        }
+    }
+    private fun getExpense(){
+        val preferences = getSharedPreferences("pref", MODE_PRIVATE)
+        var json: String = ""
+        if (!preferences.contains("json"))
+        {
+            return
+        } else {
+            json = preferences.getString("json", "NOT_JSON").toString()
+        }
+        val temp = Gson().fromJson<List<Expenses>>(json, object: TypeToken<List<Expenses>>(){}.type)
+        bookList.addAll(temp)
+    }
+}
